bcb514ee0ef3de21813b50c858daee2812f29c0e
Mubashir <khanmubashir1007@gmail.com>
2024-10-04 16:08:57 +0530

    03oct

--- Array.java
+++ Array.java
@@ -13,7 +13,22 @@ class Array {
         obj.BinaryRecursion(arr1, 0, n, Ele);
         int []arr = {1,2,3,5,6,4,2,5,2};
         int numofEleInArray = obj.removeElement(arr,2);
-
+        System.out.print("Enter Size of Array : ");
+        int k = scanner.nextInt();
+        int nums[] =new int[k];
+        for(int i = 0 ; i < k ; i++){
+            System.out.print("Enter Ele of Array : ");
+            nums[i] = scanner.nextInt();
+        }
+        obj.PrintRecuPermute(nums);
+        // the merge sorting is starting
+        System.out.println();
+        System.out.println("Merge Sort is started.");
+        int [] mergeArr = obj.Array(7);
+       mergeArr = obj.MergeSort(mergeArr, 0, 7);
+        for(int i = 0 ; i < 7 ; i++){
+            System.out.print(mergeArr[i] + "    ");
+        }
     }
 
     // Array
@@ -79,4 +94,111 @@ class Array {
         return size + 1; // Return the new size of the array
     }
 
+    public HashSet<Integer> allPermutation(int[]arr , int n,int c){
+        HashSet<Integer> res = new HashSet();
+        return res;
+    }
+
+    //Get all the possible permutation
+      // Get all possible permutations using recursion and backtracking
+      public void RecuPermute(int[] nums, List<Integer> ds, List<List<Integer>> ans, boolean[] freq) {
+        if (ds.size() == nums.length) {
+            ans.add(new ArrayList<>(ds));  // Add a new copy of ds to ans
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!freq[i]) {
+                ds.add(nums[i]);
+                freq[i] = true;  // Mark this number as used
+                RecuPermute(nums, ds, ans, freq);
+                ds.remove(ds.size() - 1);  // Backtrack
+                freq[i] = false;  // Reset the frequency
+            }
+        }
+    }
+
+    // Print all permutations
+    public void PrintRecuPermute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        boolean[] freq = new boolean[nums.length];
+        RecuPermute(nums, ds, ans, freq);
+
+        // Print each permutation
+        for (List<Integer> perm : ans) {
+            for (int num : perm) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // this function is use to make an arr and return arr;
+    public int[] Array(int n ){
+        Scanner scanner = new Scanner(System.in);
+        int arr[] = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            System.out.print("Enter number : ");
+            arr[i] = scanner.nextInt();
+        }
+        return arr;
+    }
+
+    // Merger Sort Algorithm           
+    public int[] MergeSort(int arr[] , int low , int high){
+        if (low >= high) return arr;
+
+        int mid = (low + high) / 2;
+    
+        // Sort the first half
+        MergeSort(arr, low, mid);
+        // Sort the second half
+        MergeSort(arr, mid + 1, 6);
+        // Merge the sorted halves
+        Merge(arr, low, mid, 6);
+    return arr;
+    }
+
+
+    //this function is use to merge the for mergeSort
+    public void Merge(int arr[], int low, int mid, int high) {
+        int left = low;      // Pointer for left half (low to mid)
+        int right = mid + 1; // Pointer for right half (mid+1 to high)
+    
+        // Temporary array to store merged result
+        int[] temp = new int[high - low + 1];
+        int index = 0;
+    
+        // Traverse both halves and store the smaller element into temp[]
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp[index] = arr[left];
+                left++;
+            } else {
+                temp[index] = arr[right];
+                right++;
+            }
+            index++;
+        }
+    
+        // If there are remaining elements in the left half, add them to temp[]
+        while (left <= mid) {
+            temp[index] = arr[left];
+            left++;
+            index++;
+        }
+    
+        // If there are remaining elements in the right half, add them to temp[]
+        while (right <= high) {
+            temp[index] = arr[right];
+            right++;
+            index++;
+        }
+    
+        // Copy the sorted elements back into the original array
+        for (int i = 0; i < temp.length; i++) {
+            arr[low + i] = temp[i];
+        }
+    }
+    
 }
